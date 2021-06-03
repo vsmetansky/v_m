70bede584152e1b353c8baadaf0b9c98dda3415a
v <vladsmetansky@gmail.com>
2021-06-03 23:25:40 +0300

    updated covid19 pipeline

--- v_m/flows/covid19.py
+++ v_m/flows/covid19.py
@@ -1,22 +1,24 @@
 import pandas as pd
-from prefect import Flow
+from prefect import Flow, Parameter
 from prefect.executors import LocalDaskExecutor
 
-from v_m.tasks import covid19 as cv
+from v_m.tasks import covid19 as tasks
 
 with Flow('covid19', executor=LocalDaskExecutor()) as flow:
-    period = pd.date_range(*['2020-05-03', '2021-04-03'])
-    e_df = cv.extract_epidemiological_data.map(timestamp=period)
-    r_df = cv.extract_restrictions_data(period)
-    p_df = cv.extract_population_data()
+    period = pd.date_range(*['2021-02-01', '2021-02-01'])
 
-    e_df = cv.transform_epidemiological_data.map(df=e_df)
-    r_df = cv.transform_restrictions_data(r_df)
-    p_df = cv.transform_population_data(p_df)
+    e_df = tasks.extract_epidemiological_data.map(period)
+    r_df = tasks.extract_restrictions_data(period)
+    p_df = tasks.extract_population_data()
 
-    df = cv.transform(e_df, r_df, p_df)
+    e_df = tasks.transform_epidemiological_data.map(e_df)
+    r_df = tasks.transform_restrictions_data(r_df)
+    p_df = tasks.transform_population_data(p_df)
+
+    df = tasks.transform(e_df, r_df, p_df)
+
+    _ = tasks.load(df)
 
 if __name__ == '__main__':
-    flow.register(project_name='v_m')
-
     flow.run()
+    # flow.register(project_name='v_m')
