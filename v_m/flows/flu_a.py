6ce795d4da1ca396d1864bb8c2f79f2df7b886a8
v <vladsmetansky@gmail.com>
2021-06-12 00:02:04 +0300

    updates to flu_a pipeline

--- v_m/flows/flu_a.py
+++ v_m/flows/flu_a.py
@@ -1,12 +1,13 @@
 from prefect import Flow, Parameter
 from prefect.executors import LocalDaskExecutor
 
-from v_m.tasks import flu as tasks
+from v_m.tasks import flu_a as tasks
 
-with Flow('flu', executor=LocalDaskExecutor()) as flow:
-    df = tasks.extract('200501-202105')
-    df = tasks.transform(df)
-    # tasks.load(df)
+with Flow('flu_a', executor=LocalDaskExecutor()) as flow:
+    years = [f'20{year}01-20{year+1}01' for year in range(10, 21)]
+    dfs = tasks.extract.map(years)
+    dfs = tasks.transform.map(dfs)
+    tasks.load.map(dfs)
 
 if __name__ == '__main__':
     flow.run()
--- v_m/tasks/flu_a.py
+++ v_m/tasks/flu_a.py
@@ -1,22 +1,49 @@
+import datetime
+
 import pandas as pd
 import requests
 import ujson
 from prefect import task
 
-from v_m.constants import flu as const
+from dask import dataframe as dd
+from v_m.constants import flu_a as const
 from v_m.commons.database import ElasticsearchConnector
+
+
+def iso_year_start(iso_year):
+    """The gregorian calendar date of the first day of the given ISO year"""
+    fourth_jan = datetime.date(iso_year, 1, 4)
+    delta = datetime.timedelta(fourth_jan.isoweekday() - 1)
+    return fourth_jan - delta
+
+
+def iso_to_gregorian(iso_year, iso_week, iso_day):
+    """Gregorian calendar date for the given ISO year, week and day"""
+    year_start = iso_year_start(iso_year)
+    return year_start + datetime.timedelta(days=iso_day - 1, weeks=iso_week - 1)
+
+
+def process(x):
+    year, week = int(str(x)[:4]), int(str(x)[-2:])
+    dt = iso_to_gregorian(year, week, 1)
+    return dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
 
 
 @task()
 def extract(timestamp):
-    response = requests.get(const.BASE_URL.format(','.join(const.STATE_CODES), timestamp))
+    url = const.BASE_URL.format(','.join(const.STATE_CODES), timestamp)
+    response = requests.get(url)
     data = ujson.loads(response.text).get('epidata')
-    return pd.DataFrame.from_records(data)
+    return dd.from_pandas(pd.DataFrame.from_records(data), npartitions=5)
 
 
 @task()
 def transform(df):
     df.region = df.region.str.upper()
+    df['timestamp'] = df.epiweek.map(process)
+    df = df.drop(['issue', 'release_date'], axis=1)
+    df.region = 'US-' + df.region
+    df['id_'] = df['region'] + df['timestamp']
     return df
 
 
