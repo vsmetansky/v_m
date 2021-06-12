b1836fccf9656a51d4fc3149c0189cec726f6336
v <vladsmetansky@gmail.com>
2021-06-12 16:14:30 +0300

    updated flu_a tasks

--- v_m/tasks/flu_a.py
+++ v_m/tasks/flu_a.py
@@ -1,46 +1,44 @@
-import datetime
+from datetime import date
 
 import pandas as pd
 import requests
 import ujson
 from prefect import task
-
 from dask import dataframe as dd
+
+from v_m.commons.timeseries import date_to_year_week, year_week_to_date
+from v_m.commons.validation import ValidationException, validate_date_range
 from v_m.constants import flu_a as const
 from v_m.commons.database import ElasticsearchConnector
 
 
-def iso_year_start(iso_year):
-    """The gregorian calendar date of the first day of the given ISO year"""
-    fourth_jan = datetime.date(iso_year, 1, 4)
-    delta = datetime.timedelta(fourth_jan.isoweekday() - 1)
-    return fourth_jan - delta
-
-
-def iso_to_gregorian(iso_year, iso_week, iso_day):
-    """Gregorian calendar date for the given ISO year, week and day"""
-    year_start = iso_year_start(iso_year)
-    return year_start + datetime.timedelta(days=iso_day - 1, weeks=iso_week - 1)
-
-
-def process(x):
-    year, week = int(str(x)[:4]), int(str(x)[-2:])
-    dt = iso_to_gregorian(year, week, 1)
-    return dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
+@task()
+def process_parameters(start, stop):
+    try:
+        start, stop = date.fromisoformat(start), date.fromisoformat(stop)
+    except (TypeError, ValueError) as e:
+        raise ValidationException(e)
+    validate_date_range(start, stop)
+    print(date_to_year_week(start), date_to_year_week(stop))
+    return f'{date_to_year_week(start)}-{date_to_year_week(stop)}'
 
 
 @task()
-def extract(timestamp):
-    url = const.BASE_URL.format(','.join(const.STATE_CODES), timestamp)
+def extract(date_range):
+    url = const.BASE_URL.format(','.join(const.STATE_CODES), date_range)
     response = requests.get(url)
     data = ujson.loads(response.text).get('epidata')
+    if not data:
+        return pd.DataFrame()
     return dd.from_pandas(pd.DataFrame.from_records(data), npartitions=5)
 
 
 @task()
 def transform(df):
+    if len(df.columns) == 0:
+        return df
     df.region = df.region.str.upper()
-    df['timestamp'] = df.epiweek.map(process)
+    df['timestamp'] = df.epiweek.map(year_week_to_date)
     df = df.drop(['issue', 'release_date'], axis=1)
     df.region = 'US-' + df.region
     df['id_'] = df['region'] + df['timestamp']
