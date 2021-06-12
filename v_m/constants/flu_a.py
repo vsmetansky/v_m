971b92a30bea665b1ae72e0d4d1496862a8f33ec
v <vladsmetansky@gmail.com>
2021-06-12 16:26:29 +0300

    added scheduling flu_a pipeline

--- v_m/constants/flu_a.py
+++ v_m/constants/flu_a.py
@@ -6,3 +6,5 @@ COLUMNS_TYPES = dict()
 STATE_CODES = ('al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'dc', 'de', 'fl', 'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks',
                'ky', 'la', 'me', 'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 'nj', 'nm', 'ny', 'nc',
                'nd', 'oh', 'ok', 'or', 'pa', 'ri', 'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy')
+
+WEEKS_LOOKBACK = 100
--- v_m/flows/flu_a.py
+++ v_m/flows/flu_a.py
@@ -1,13 +1,45 @@
-from prefect import Flow, Parameter
+from datetime import datetime, timedelta
+
+from prefect import Flow
+from prefect.core.parameter import Parameter
 from prefect.executors import LocalDaskExecutor
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
 
 from v_m.tasks import flu_a as tasks
+from v_m.constants import flu_a as const
 
-with Flow('flu_a', executor=LocalDaskExecutor()) as flow:
-    years = [f'20{year}01-20{year+1}01' for year in range(10, 21)]
-    dfs = tasks.extract.map(years)
-    dfs = tasks.transform.map(dfs)
-    tasks.load.map(dfs)
+
+datetime_now = datetime.utcnow()
+date_stop = datetime_now.date()
+date_start = date_stop - timedelta(weeks=const.WEEKS_LOOKBACK)
+date_stop_serializable = date_stop.isoformat()
+date_start_serializable = date_start.isoformat()
+
+start = Parameter(name='start', default=date_start_serializable)
+stop = Parameter(name='stop', default=date_stop_serializable)
+
+clock = IntervalClock(
+    start_date=datetime_now,
+    interval=timedelta(weeks=1),
+    parameter_defaults={
+        start.name: date_start_serializable,
+        stop.name: date_stop_serializable
+    }
+)
+
+schedule = Schedule(clocks=[clock])
+executor = LocalDaskExecutor()
+
+with Flow(
+        name='flu_a',
+        executor=executor,
+        schedule=schedule
+) as flow:
+    date_range = tasks.process_parameters(start, stop)
+    df = tasks.extract(date_range)
+    df = tasks.transform(df)
+    tasks.load(df)
 
 if __name__ == '__main__':
     flow.run()
