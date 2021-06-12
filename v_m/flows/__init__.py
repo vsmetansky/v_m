09c0fba788db8758846c51290f3aeec99b4d5821
v <vladsmetansky@gmail.com>
2021-06-12 19:48:04 +0300

    fixed schedule initialization

--- v_m/flows/__init__.py
+++ v_m/flows/__init__.py
@@ -7,17 +7,18 @@ from prefect.schedules.clocks import IntervalClock
 
 
 def init_dates(**lookback: int) -> Tuple:
-    date_stop = datetime.utcnow().date()
+    date_now = datetime.utcnow()
+    date_stop = date_now.date()
     date_start = date_stop - timedelta(**lookback)
     date_stop_serializable = date_stop.isoformat()
     date_start_serializable = date_start.isoformat()
-    return date_stop, date_start, date_stop_serializable, date_start_serializable
+    return date_now, date_stop, date_start, date_stop_serializable, date_start_serializable
 
 
 def init_date_parameters(
         dates: Tuple
 ) -> Tuple[Parameter, Parameter]:
-    _, _, date_stop_serializable, date_start_serializable = dates
+    _, _, _, date_stop_serializable, date_start_serializable = dates
     return (
         Parameter(name='start', default=date_start_serializable),
         Parameter(name='stop', default=date_stop_serializable)
@@ -25,9 +26,9 @@ def init_date_parameters(
 
 
 def init_schedule(start, stop, dates: Tuple, **interval: int) -> Schedule:
-    start_date, _, date_stop_serializable, date_start_serializable = dates
+    date_now, _, _, date_stop_serializable, date_start_serializable = dates
     clock = IntervalClock(
-        start_date=start_date,
+        start_date=date_now,
         interval=timedelta(**interval),
         parameter_defaults={
             start.name: date_start_serializable,
